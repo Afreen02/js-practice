6702077df764e12387641ccfdefc1a9e52c42a62
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-04-30 20:22:32 +0000

    datatypes

---
+++ .devcontainer/01_basicsofjavascript/02_datatype.js
@@ -0,0 +1,14 @@
+"use strict" //treat all js code as newer version
+let name = "Afreen"
+let age = 25
+let isLoggedIn = false
+//number => 2 to pwr 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//object
+
+console.log(typeof"Afreen");
+//null error gives undefined and undefined gives only undefined
